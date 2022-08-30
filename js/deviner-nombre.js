544efed032d737226f8ef93d77dbe23349e88894
Xavier Rossi <xaviermagic90@gmail.com>
2022-08-30 16:12:37 +0200

    1st Test

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,13 +1,13 @@
 /**
 * Jeu du nombre mystère
-* @author  Steve Fallet
+* @author  Xavier Rossi
 * @version 0.1
-* @since   2018-09-09 (date de création)
+* @since   2022-08-30
 */
 
 // Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
 (function main() {
-    "use strict";
+    'use strict';
 
     /**
      * Retourne un nombre entier aléatoire compris entre min et max
@@ -18,5 +18,18 @@
     function tireNombre(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
     }
-
+    const LENOMBRE = tireNombre(0, 100);
+    let numberEquals = false, essai = 0;
+    while (!numberEquals){
+        let number = prompt('Donne moi donc un nombre');
+        if (number > LENOMBRE){
+            window.alert('Ton nombre est trop grand');
+        } else if (number < LENOMBRE) {
+            window.alert('Ton nombre est trop petit');
+        } else {
+            numberEquals = true;
+            window.alert('Wp après ' + essai + ' essai(s) bg !');
+        }
+        essai++;
+    }
 }()); // main IIFE
